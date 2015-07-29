632583e136f45158ac9aac4234723a7b582d5a55
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-07-29 11:54:05 +0200

    Correct package name in setup.find_packages.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='evt',
     version='2015.07.19.1',
-    packages=find_packages(include=('src*',)),
+    packages=find_packages(include=('evt*',)),
     entry_points={
         'console_scripts': 'evt_standalone = evt.standalone:main'
     }

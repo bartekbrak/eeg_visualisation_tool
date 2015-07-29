d1212a38e16d447d23090c20bed5aeafca8ed6ee
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-07-29 11:28:13 +0200

    Add dynamic y range calculation.
    For now on Python side only. We'll need to come up with BokehJS version
    too.

--- evt/standalone.py
+++ evt/standalone.py
@@ -5,6 +5,7 @@ from bokeh.models import ColumnDataSource, Range1d, HoverTool, TapTool, \
 from bokeh.embed import file_html
 from bokeh.resources import INLINE
 from bokeh.plotting import figure
+import itertools
 from jinja2 import Environment, PackageLoader
 
 from evt.constants import column_name_map
@@ -25,15 +26,15 @@ def get_tools(hover_js_, tap_js_, progress_bar_name):
     )
 
 
-def get_figure(tools, video_len):
+def get_figure(tools, video_len, **kwargs):
     f = figure(
         tools=tools,
         toolbar_location=None,
         width=640,
         height=200,
         x_range=Range1d(1, video_len),
-        y_range=Range1d(0, 1),
         x_axis_type='datetime',
+        **kwargs
     )
     f.ygrid.grid_line_alpha = 0.1
     f.xgrid.grid_line_alpha = 0.2
@@ -75,22 +76,31 @@ def get_mean(data):
     return arithmetic_mean(*sub_means)
 
 
+def get_data_min_max(grouped_data, key='y_series'):
+    list_of_lists = [one[key] for one in grouped_data.values()]
+    flat = list(itertools.chain(*list_of_lists))
+    return min(flat), max(flat)
+
+
 def main():
     data = get_from_csv('tomek.csv', column_name_map)
-    # grouped_by = ('age', 'sex', 'favourite_brand')
-    grouped_plot_data = group_by(('age',), data)
+    grouped_by = ('age', 'sex', 'favourite_brand')
+    grouped_plot_data = group_by(grouped_by, data)
     video_len = 10100
     sampling_rate = 333
     video_filename = 'myvideo.mp4'
-
+    ymin, ymax = get_data_min_max(grouped_plot_data)
+    ymargin = 0.2
     progress_bar = get_progress_bar()
     f = get_figure(
         tools=get_tools(hover_js, tap_js, progress_bar.name),
-        video_len=video_len
+        video_len=video_len,
+        y_range=Range1d(ymin - ymargin, ymax + ymargin)
     )
     f2 = get_figure(
         tools=get_tools(hover_js, tap_js, progress_bar.name),
-        video_len=video_len
+        video_len=video_len,
+        y_range=Range1d(ymin - ymargin, ymax + ymargin)
     )
     f.line('x', 'y', source=progress_bar, line_color='green')
     f2.line('x', 'y', source=progress_bar, line_color='green')

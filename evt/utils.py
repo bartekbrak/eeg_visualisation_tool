5d335fd9db34b5a38d7260be56a1853e53bda93c
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-07-31 19:28:33 +0200

    Improve and dry average_yaxis_by_properties
    - Add docstrings.
    - Dry common code.

--- evt/utils.py
+++ evt/utils.py
@@ -6,24 +6,37 @@ from evt.constants import data_column_name
 
 
 def average_yaxis_by_properties(column_names, rows):
+    """
+    This will produce tree-like values combining all column_names.
+    rows will be grouped by first column_name, then every result by next,
+    and so on.
+    The result has depth of len(column_names).
+    Not used directly in the application but left for extensibility.
+    """
     # TODO: find out if numpy can do this
-    sorted_by_column_name = sorted(rows, key=itemgetter(*column_names))
+    sort_key = itemgetter(*column_names)
+    sorted_by_column_name = sorted(rows, key=sort_key)
     grouper = itertools.groupby(
-        sorted_by_column_name, itemgetter(*column_names)
+        sorted_by_column_name, sort_key
     )
     for column_values, grouped_data_rows in grouper:
-        description = _get_description(column_names, column_values)
+        description = dict(zip(column_names, column_values))
         grouped = mean(
             [data_row[data_column_name] for data_row in grouped_data_rows],
             axis=0
         )
-        yield description, grouped
+        yield description, grouped, column_names
 
 
-def _get_description(column_names, column_values):
-    if len(column_names) == 1:
-        column_values = (column_values,)
-    return dict(zip(column_names, column_values))
+def average_yaxis_by_properties_separate(column_names, rows):
+    """
+    This will produce a group for each column_name without combining them.
+    The result has depth of 1.
+    Note that we want to pass the tuple.
+    """
+    for column_name in column_names:
+        for _ in average_yaxis_by_properties((column_name, ), rows):
+            yield _
 
 
 def get_random_colour():

07d0f7592879b65f2a150d1b46852d510bd71099
Bartek Brak <bartek.rychlicki@gmail.com>
2015-11-14 17:28:17 +0100

    Comment javascript

--- evt/models.py
+++ evt/models.py
@@ -10,4 +10,7 @@ class Line(object):
         self.min = min
         self.max = max
         self.mean = mean
+        # hold a copy of all values of a plot, doubled but this is the only way
+        # the value can be accessed as there is no way you could read it
+        # from the plot itself
         self.continuum = continuum
